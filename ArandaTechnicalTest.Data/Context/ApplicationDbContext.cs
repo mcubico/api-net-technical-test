edf09420c5c0bc3f8eecbb4956759555260f86f3
Mauricio Montoya Medrano <mcubico33@gmail.com>
2022-10-28 07:23:30 -0500

    Inicio con la construcción de la bd usando code first

---
+++ ArandaTechnicalTest.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ArandaTechnicalTest.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArandaTechnicalTest.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
