8f171c6aee37410010075f522ec90fa2c9cb49e8
Mauricio Montoya Medrano <mcubico33@gmail.com>
2022-10-29 09:59:03 -0500

    Implemento los endpoints requeridos para la prueba

--- ArandaTechnicalTest.Presentation/Controllers/ProductsController.cs
+++ ArandaTechnicalTest.Presentation/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using ArandaTechnicalTest.Data.Entities;
+﻿using ArandaTechnicalTest.Data.Context;
+using ArandaTechnicalTest.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArandaTechnicalTest.Presentation.Controllers
 {
@@ -7,26 +9,55 @@ namespace ArandaTechnicalTest.Presentation.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Product>> Get()
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
         {
-            return new List<Product>()
-            {
-                new Product()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Pantalón bota ancha",
-                    Category = "Pants",
-                    Description ="Pantalón de bota ancha",
-                },
-                new Product()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Blusa campesina",
-                    Category = "Blouses",
-                    Description ="Blusa campesina"
-                }
-            };
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Product data)
+        {
+            _context.Add(data);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult> Put(Product data, Guid id)
+        {
+            if (data.Id != id)
+                return BadRequest("Los identificadores del producto no coinciden");
+
+            bool exists = await _context.Products.AnyAsync(p => p.Id == id);
+            if (!exists)
+                return NotFound();
+
+            _context.Update(data);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            bool exists = await _context.Products.AnyAsync(p => p.Id == id);
+            if (!exists)
+                return NotFound();
+
+            _context.Remove(new Product() { Id = id});
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
